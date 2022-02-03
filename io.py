cba421412de24e0882f2ec5c76e1e731970c0f41
MickaelRigault <mick.rigault@gmail.com>
2022-02-03 14:35:47 +0100

    adding first io.py

--- io.py
+++ io.py
@@ -7,7 +7,9 @@ def get_lcfit_datafile(lcfit_rootdir, input_version="SK",
     """ """
     import pandas
     "/sps/ztf/users/nnicolas/Data/sims/NN_COMBINE_VSIZE/2_LCFIT"
-    rootfiles = glob(os.path.join(lcfit_rootdir, f'*D_*_SIM-{input_version}/output/PIP_{input_version}_VSIZE_{input_version}_*_DATA-{fitted_version}-*/FITOPT000.ROOT')
+    rootfiles = glob( os.path.join(lcfit_rootdir,
+                           f'*D_*_SIM-{input_version}/output/PIP_{input_version}_VSIZE_{input_version}_*_DATA-{fitted_version}-*/FITOPT000.ROOT')
+                           )
     datafile = pandas.DataFrame(rootfiles, columns=["fullpath"])
     datafile[["sourcedir","outdir"]] = datafile["fullpath"].str.split("/", expand=True, )[[9,11]]
 
