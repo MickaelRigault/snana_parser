b5a855b92511b340aea5884021eabd395ff346e9
MickaelRigault <mick.rigault@gmail.com>
2022-02-03 16:42:09 +0100

    adding io datafiles

--- io.py
+++ io.py
@@ -83,7 +83,7 @@ class _DataFileIO_():
         gp = self.datafile.groupby(gkeys)["fullpath"].apply(list)
 
         if len(gkeys) == 1:
-            return gp.loc[gkeys]
+            return gp
         
 
         lkeys, values = np.asarray([[k,v] for k,v in local.items() if v is not None and v not in ["*","all"]],
