15488bee056ef7dfcbd15f30f98d47b812a643e7
Nora NICOLAS <n.nicolas@ipnl.in2p3.fr>
2022-02-11 15:33:04 +0100

    New build_merged with survey name

--- simulations.py
+++ simulations.py
@@ -1,29 +1,39 @@
+import dask
+import pandas as pd
+import simulations
 
-
-import pandas
-
-from .io import LCFitIO
+from .utils import root
+from .io import LCFitIO, SNANA__OUT_ROOTDIR
 
 
 def get_merged_simdata_file(true_model):
     """ """
-    return LCFitIO.get_datafile_filename().replace("datafile", f"truemodel-{true_model}")
-
-def build_merged_simdata(true_model, client, fileout = None, makedir=True):
-    """ """
-    from .utils.root import read_snana_root
-    if fileout is None:
-        fileout = get_merged_simdata_file(true_model)
-
-    extension = fileout.split(".")[-1]
-    if makedir:
-        dirname = os.path.dirname(fileout)
-        os.makedirs(dirname, exist_ok=True)
-    
-    
-    files = LCFitIO.fetch_filepaths(true_model=true_model)
-    f_df = client.map(read_snana_root, files, keys="FITRES", use_dask=False)
-    dd_ = pandas.concat(client.gather(f_df) )
-    return getattr(dd_,f"to_{extension}")( fileout )
+    return LCFitIO.get_datafile_filename().replace("datafile",
+                                                   f"truemodel-{true_model}")
 
 
+def build_merged_simdata(true_model, as_dask=None):
+    """as_dask: None --> returns nothing and stores
+                'compute' --> returns the multi-index dataframe
+                'delayed' --> returns the delayed dataframe"""
+    mdatafile = pd.read_parquet(SNANA__OUT_ROOTDIR +
+                                '/2_LCFIT/merged_simdata_datafile.parquet')
+    tmodel_datafile = mdatafile.query(f"true_model=='{true_model}'")
+    surveys = tmodel_datafile.groupby('survey')['fullpath'].apply(list)
+    d_cdf = []
+    for survey in surveys.index:
+        d_df = []
+        fid = []
+        for file_ in surveys[survey]:
+            fid.append(int(file_.split('/')[-2].split('-')[-1]))
+            d_df.append(dask.delayed(root.read_snana_root)(file_))
+
+        d_cdf.append(dask.delayed(pd.concat)(d_df, keys=fid))
+    d_cdf_t = dask.delayed(pd.concat)(d_cdf, keys=surveys.index)
+    if as_dask == 'delayed':
+        return d_cdf_t
+    cdf_t = d_cdf_t.compute()
+    if as_dask == 'compute':
+        return cdf_t
+    filename = simulations.get_merged_simdata_file(true_model)
+    return cdf_t.to_parquet(filename)
